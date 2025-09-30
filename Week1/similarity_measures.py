72c9e7e15dd984ef67f4e641ad1c758ee23702e8
trotuj1 <shintomf2003@gmail.com>
2025-09-30 21:05:04 +0200

    feature: general function for distance

--- Week1/similarity_measures.py
+++ Week1/similarity_measures.py
@@ -15,13 +15,37 @@ def histogram_intersection_distance(a, b):
 def hellinger_kernel_distance(a, b):
     return np.sum(np.sqrt(a * b))
 
+def calculate_distance(a, b, metric:str) -> float:
+    """
+    Calculate distance of histogram a and b from a given metric.
+
+    Args:
+        a: 1D array histogram
+        b: 1D array histogram
+        metric: distance function of choice     
+    """
+    if metric == "euclidean":
+        result = euclidean_distance(a, b)
+    elif metric == "l1":
+        result = l1_distance(a, b)
+    elif metric == "chi2":
+        result = chi2_distance(a, b)
+    elif metric == "histogram_intersection":
+        result = histogram_intersection_distance(a, b)
+    elif metric == "hellinger":
+        result = hellinger_kernel_distance(a, b)
+    else:
+        raise ValueError(f"Unknown metric: {metric}")
+    
+    return result
+
 
 # if __name__ == "__main__":
 #     a = np.array([1,2,3,4], dtype=np.float32)
 #     b = np.array([5,6,7,8], dtype=np.float32)
 
-#     print("Euclidean Distance: ", euclidean_distance(a,b))
-#     print("L1 Distance: ", l1_distance(a,b))
-#     print("Chi2 Distance: ", chi2_distance(a,b))
-#     print("Histogram Intersection Distance: ", histogram_intersection_distance(a,b))
-#     print("Hellinger Kernel Distance: ", hellinger_kernel_distance(a,b))
+#     print("Euclidean Distance: ", calculate_distance(a,b,"euclidean"))
+#     print("L1 Distance: ", calculate_distance(a,b,"l1"))
+#     print("Chi2 Distance: ",  calculate_distance(a,b,"chi2"))
+#     print("Histogram Intersection Distance: ", calculate_distance(a,b,"histogram_intersection"))
+#     print("Hellinger Kernel Distance: ", calculate_distance(a,b,"hellinger"))
