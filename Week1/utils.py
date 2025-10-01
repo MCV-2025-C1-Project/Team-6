c9f03a15f0203d9d27a666852e9010a8310b61e0
urijus <orioljuan28@gmail.com>
2025-10-01 08:27:09 +0200

    [NEW] general equalization

--- Week1/utils.py
+++ Week1/utils.py
@@ -1,3 +1,4 @@
+"This script contains utils used throughout the project."
 import random
 import numbers
 from typing import Iterable, List, Tuple, Optional
@@ -20,7 +21,8 @@ def histogram(
         - probs (bool) Return the PDF (True) or the counts (False).
 
     Returns:
-        - counts (list): Number of pixels inside a bin. Returns probs or counts.
+        - counts (list): Number of pixels inside a bin.
+        - pdf (list): Bin probabilities if probs=True, else None.
         - edges (list): The indexes of the edges of each bin.
     """
     if not data:
@@ -57,8 +59,10 @@ def histogram(
     # Compute PDF
     if probs:
         n = len(data)
-        counts = [c / (n * width) for c in counts]
-    return counts, edges
+        pdf = [c / n for c in counts]
+        return counts, pdf, edges
+    
+    return counts, None, edges
 
 def plot_histogram(
     data: Iterable[numbers.Real],
@@ -81,14 +85,14 @@ def plot_histogram(
     Returns:
         - outfile (str): The path to save file.
     """
-    counts, edges = histogram(data, n_bins=n_bins, data_range=data_range, probs=probs)
+    counts, pdf, edges = histogram(data, n_bins=n_bins, data_range=data_range, probs=probs)
 
     # Draw bars
     widths = [edges[i+1] - edges[i] for i in range(len(edges)-1)]
     lefts  = edges[:-1]
 
     fig, ax = plt.subplots()
-    ax.bar(lefts, counts, width=widths, align="edge")
+    ax.bar(lefts, pdf if probs else counts, width=widths, align="edge")
     ax.set_title(title)
     ax.set_xlabel(xlabel)
     ax.set_ylabel("Probability density" if probs else "Count")
@@ -96,18 +100,42 @@ def plot_histogram(
 
     if outfile:
         fig.savefig(outfile, dpi=200)
-
     plt.show()
     return outfile
+
+def equalize_histogram(probs: list):
+    """
+    Implement histogram equalization based on the class' slides.
+    """
+    num_bins = len(probs)
+    assert abs(sum(probs) - 1.0) < 1e-6, "Input must be a PDF that sums to 1."
+    mapping = [0]*(num_bins)
+    cdf = 0.0
+
+    for idx in range(num_bins):
+        cdf += probs[idx]
+        t = round((num_bins - 1) * cdf)
+        if t < 0: t = 0
+        if t > num_bins - 1: 
+            t = num_bins - 1
+
+        mapping[idx] = int(t) # mapping between old and new hist
+
+    out = [0.0] * len(probs)
+    for k, p in enumerate(probs):
+        out[mapping[k]] += p
+    return out, mapping
 
 
 if __name__=="__main__":
     # Example
+
     # Create test data
     random.seed(42)
     data = [random.gauss(0, 1) for _ in range(70)] + \
             [random.gauss(3, 0.8) for _ in range(30)]
 
+    # Plot histogram
     path = plot_histogram(
         data,
         n_bins=10,
@@ -117,3 +145,7 @@ if __name__=="__main__":
         xlabel="Value",
         outfile=None
     )
+
+    counts, edges = histogram(data)
+
+    print(counts, edges)
