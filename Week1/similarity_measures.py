6c6afca07fe9ed7a1140a670ea3d760c7f7f3793
Alvaritox11 <alvaro11.8.03@gmail.com>
2025-10-01 20:44:44 +0200

    Feature: cosine and bhattacharyya distances implemented. Bhatta pending to test

--- Week1/similarity_measures.py
+++ Week1/similarity_measures.py
@@ -1,36 +1,57 @@
 import numpy as np
+import cv2
 
-def euclidean_distance(a, b):
-	"""
-	Compute the Euclidean (L2) distance between two vectors.
-	"""
+
+def euclidean_distance(a: np.ndarray, b: np.ndarray) -> float:
+    """
+    Compute the Euclidean (L2) distance between two vectors.
+    """
     return np.sqrt(np.sum((a - b) ** 2))
 
-def l1_distance(a, b):
+def l1_distance(a: np.ndarray, b: np.ndarray) -> float:
     """
-	Compute the Manhattan (L1) distance between two vectors.
-	"""
-	return np.sum(np.abs(a - b))
-
-def chi2_distance(a, b):
+    Compute the Manhattan (L1) distance between two vectors.
     """
-	Compute the Chi-square distance between two vectors.
-	"""
-	return np.sum(((a - b) ** 2) / (a + b))
+    return np.sum(np.abs(a - b))
 
-def histogram_intersection_distance(a, b):
+def chi2_distance(a: np.ndarray, b: np.ndarray) -> float:
     """
-	Compute the histogram intersection similarity.
-	"""
-	return np.sum(np.minimum(a,b))
-
-def hellinger_kernel_distance(a, b):
+    Compute the Chi-square distance between two vectors.
     """
-	Compute the Hellinger kernel similarity.
-	"""
-	return np.sum(np.sqrt(a * b))
+    return np.sum(((a - b) ** 2) / (a + b))
 
-def calculate_distance(a, b, metric:str) -> float:
+def histogram_intersection_distance(a: np.ndarray, b: np.ndarray) -> float:
+    """
+    Compute the histogram intersection similarity.  
+    """
+    return np.sum(np.minimum(a,b))
+
+def hellinger_kernel_distance(a: np.ndarray, b: np.ndarray) -> float:
+    """
+    Compute the Hellinger kernel similarity.
+    """
+    return np.sum(np.sqrt(a * b))
+
+def cosine_distance(a: np.ndarray, b: np.ndarray) -> float:
+    """
+    Compute the cosine distance. 
+    """
+    return 1 - (np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b)))
+
+def bhattacharyya_distance(a: np.ndarray, b: np.ndarray) -> float:
+    """
+    Compute the bhattacharyya distance. Used for probability distributions.
+    """
+    # Necessary to valid probability distributions ?? Like this -> v = v / np.sum(v)
+
+    # Bhattacharrya coefficient
+    # bc = np.sum(np.sqrt(a * b))
+    
+    # return -np.log(bc)
+    return cv2.compareHist(a,b, cv2.HISTCMP_BHATTACHARYYA)
+
+
+def calculate_distance(a: np.ndarray, b: np.ndarray, metric:str) -> float:
     """
     Calculate distance of histogram a and b from a given metric.
 
@@ -49,6 +70,10 @@ def calculate_distance(a, b, metric:str) -> float:
         result = histogram_intersection_distance(a, b)
     elif metric == "hellinger":
         result = hellinger_kernel_distance(a, b)
+    elif metric == "cosine":
+        result = cosine_distance(a, b)
+    elif metric == "bhattacharyya":
+        result = bhattacharyya_distance(a, b)
     else:
         raise ValueError(f"Unknown metric: {metric}")
     
@@ -64,3 +89,5 @@ def calculate_distance(a, b, metric:str) -> float:
 #     print("Chi2 Distance: ",  calculate_distance(a,b,"chi2"))
 #     print("Histogram Intersection Distance: ", calculate_distance(a,b,"histogram_intersection"))
 #     print("Hellinger Kernel Distance: ", calculate_distance(a,b,"hellinger"))
+#     print("Cosine Distance: ", calculate_distance(a,b,"cosine"))
+#     print("Bhattacharyya Distance: ", calculate_distance(a,b,"bhattacharyya"))
