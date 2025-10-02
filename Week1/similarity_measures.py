eed1f6f575ecf0d8cdf76f6774df26c32c6f17cf
Diiego-H <diegohernandezanton@gmail.com>
2025-10-02 23:10:36 +0200

    Similarities from collection of histograms

--- Week1/similarity_measures.py
+++ Week1/similarity_measures.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+from typing import List
 
 
 def euclidean_distance(a: np.ndarray, b: np.ndarray) -> float:
@@ -51,7 +52,7 @@ def bhattacharyya_distance(a: np.ndarray, b: np.ndarray) -> float:
     return cv2.compareHist(a,b, cv2.HISTCMP_BHATTACHARYYA)
 
 
-def calculate_distance(a: np.ndarray, b: np.ndarray, metric:str) -> float:
+def calculate_distance(a: np.ndarray, b: np.ndarray, metric: str) -> float:
     """
     Calculate distance of histogram a and b from a given metric.
 
@@ -78,6 +79,15 @@ def calculate_distance(a: np.ndarray, b: np.ndarray, metric:str) -> float:
         raise ValueError(f"Unknown metric: {metric}")
     
     return result
+
+
+def compute_similarities(l1: List[np.ndarray], l2: List[np.ndarray], metric: str = "euclidean") -> np.ndarray:
+    # TODO: Improve the explanation, returns the similarities.
+    similarities = np.zeros(shape=(len(l1), len(l2)))
+    for i, descriptors1 in enumerate(l1):
+        for j, descriptors2 in enumerate(l2):
+            similarities[i,j] = calculate_distance(descriptors1, descriptors2, metric)
+    return similarities
 
 
 # if __name__ == "__main__":
