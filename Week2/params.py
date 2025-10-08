3ede6f4b07079a0cc7d8517dc53e0592354cd3f3
urijus <orioljuan28@gmail.com>
2025-10-08 15:52:20 +0200

    [NEW] New experiments for background removing

--- Week2/params.py
+++ Week2/params.py
@@ -1,4 +1,6 @@
 """This scripts contains parameters for Week's 1 experiments."""
+
+# TODO:  Do we need all of those?
 experiments = {
     "k_values": [1,5],
     "methods": ["hsv"], 
@@ -17,4 +19,9 @@ best_config2 = {
     "method": "rgb-hs", 
     "n_bins": 16,
     "metric": "l1"
+}
+
+background_experiments = {
+    "struct" = [(3,3), (4,4), (5,5)]
+    #TODO: Add wanted experiments for gridsearch
 }
